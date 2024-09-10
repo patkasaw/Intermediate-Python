1ffe277f05646731dd2c12f39af74e4ed1fec3c7
patkasaw <patrycja.sawicka52@gmail.com>
2024-09-10 14:40:39 +0200

    example of sets

---
+++ sets.py
@@ -0,0 +1,16 @@
+my_fruits = {'apple', 'banana', 'orange'}
+friend_fruits = {'pineapple', 'kiwi', 'lime'}
+
+union_result = my_fruits.union(friend_fruits)
+
+print(union_result)
+
+intersection_result = my_fruits.intersection(friend_fruits)
+
+print(intersection_result)
+
+difference_result = my_fruits - friend_fruits
+
+print(difference_result)
+
+
