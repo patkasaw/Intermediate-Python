ef04c0d3d5a9fa9487dc2aeef793e841019aa594
patkasaw <patrycja.sawicka52@gmail.com>
2024-09-11 13:36:43 +0200

    accesing dicitionary elements

---
+++ players.py
@@ -0,0 +1,6 @@
+players_data = [
+    {'name': 'Patrick Mahomes', 'position': 'Quarterback', 'jersey_number': 12, 'yards_gained': 140, 'touchdowns': 0},
+    {'name': 'Tyreek Hill', 'position': 'Wide Receiver', 'jersey_number': 10, 'yards_gained': 150, 'touchdowns': 2},
+    {'name': 'Travis Kelce', 'position': 'Tight End', 'jersey_number': 13, 'yards_gained': 170, 'touchdowns': 3},
+]
+print(players_data)
